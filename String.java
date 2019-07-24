d8379f72dad4461279b850d91803bb3c06b0d607
Dipesh Bhatevera <35795916+DEEPESH98@users.noreply.github.com>
2019-07-24 11:54:11 +0530

    How to add full sentence in variable.

---
+++ String.java
@@ -0,0 +1,16 @@
+package com.Dipesh.javaapp;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       String a;
+
+	Scanner j = new Scanner (System.in);
+	a=j.nextLine();
+
+	    System.out.println(a);
+
+    }
+}
